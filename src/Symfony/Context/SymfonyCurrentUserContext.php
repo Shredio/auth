41ef9ac6ec5ef63eaa04216e6c28d587731363c7
MartkCz <martk494@gmail.com>
2025-10-03 18:33:49 +0200

    chore: better message for dev

--- src/Symfony/Context/SymfonyCurrentUserContext.php
+++ src/Symfony/Context/SymfonyCurrentUserContext.php
@@ -5,6 +5,7 @@ namespace Shredio\Auth\Symfony\Context;
 use Shredio\Auth\Context\CurrentUserContext;
 use Shredio\Auth\Entity\UserEntity;
 use Shredio\Auth\Exception\ForbiddenException;
+use Shredio\Auth\Exception\LogicException;
 use Shredio\Auth\Requirement\Requirement;
 use Shredio\Auth\UserRequirementChecker;
 use Symfony\Bundle\SecurityBundle\Security;
@@ -26,7 +27,10 @@ final readonly class SymfonyCurrentUserContext implements CurrentUserContext
 			return null;
 		}
 
-		assert($user instanceof UserEntity);
+		if (!$user instanceof UserEntity) {
+			throw new LogicException(sprintf('Current user %s is does not implement %s.', $user::class, UserEntity::class));
+		}
+
 		return $user;
 	}
 
